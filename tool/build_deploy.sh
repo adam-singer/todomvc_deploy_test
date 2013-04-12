db7090ae1d59ee81f5172475bd4011fefd94da21
adam <financeCoding@gmail.com>
2013-04-11 20:23:59 -0700

    adding pub install

--- tool/build_deploy.sh
+++ tool/build_deploy.sh
@@ -1,3 +1,4 @@
+pub install && 
 dart tool/hop_runner.dart --log-level all clean &&
 dart tool/hop_runner.dart --log-level all build dart2js &&
 dart tool/hop_runner.dart --log-level all dart2js && 
